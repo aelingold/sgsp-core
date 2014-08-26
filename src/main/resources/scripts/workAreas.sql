f1148412baf442d18c212f045f8ad74c0aebcc9c
Alexis Elingold <aelingold@despegar.com>
2014-08-26 17:53:45 -0300

    added sql scripts

---
+++ src/main/resources/scripts/workAreas.sql
@@ -0,0 +1,7 @@
+INSERT INTO `singuia`.`work_areas` (`created_at`,`description`,`is_enabled`,`name`,`updated_at`) VALUES (curdate(),null,true,'Gasista',null);
+INSERT INTO `singuia`.`work_areas` (`created_at`,`description`,`is_enabled`,`name`,`updated_at`) VALUES (curdate(),null,true,'Pintor',null);
+INSERT INTO `singuia`.`work_areas` (`created_at`,`description`,`is_enabled`,`name`,`updated_at`) VALUES (curdate(),null,true,'Plomero',null);
+INSERT INTO `singuia`.`work_areas` (`created_at`,`description`,`is_enabled`,`name`,`updated_at`) VALUES (curdate(),null,true,'Electricista',null);
+INSERT INTO `singuia`.`work_areas` (`created_at`,`description`,`is_enabled`,`name`,`updated_at`) VALUES (curdate(),null,true,'Cerrajero',null);
+INSERT INTO `singuia`.`work_areas` (`created_at`,`description`,`is_enabled`,`name`,`updated_at`) VALUES (curdate(),null,true,'Albañil',null);
+INSERT INTO `singuia`.`work_areas` (`created_at`,`description`,`is_enabled`,`name`,`updated_at`) VALUES (curdate(),null,true,'Flete',null);
