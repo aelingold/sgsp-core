a5c07ef3d08b52511862c9b3f7c6d59c63f1415a
Alexis Elingold <aelingold@despegar.com>
2014-10-06 13:17:17 -0300

    fixed test

--- src/test/java/org/ucema/sgsp/test/UserTest.java
+++ src/test/java/org/ucema/sgsp/test/UserTest.java
@@ -7,6 +7,7 @@ import org.ucema.sgsp.BaseTest;
 import org.ucema.sgsp.api.dto.RegistrationDTO;
 import org.ucema.sgsp.api.dto.UserDTO;
 import org.ucema.sgsp.exception.DuplicateEmailException;
+import org.ucema.sgsp.security.model.SocialMediaService;
 import org.ucema.sgsp.security.model.User;
 import org.ucema.sgsp.service.UserService;
 
@@ -32,8 +33,30 @@ public class UserTest extends BaseTest {
 		User response = userService.registerNewUserAccount(userAccountData);
 
 		UserDTO userRetrieved = userService.get(response.getId());
+
+		Assert.assertNull(userRetrieved);
+	}
+	
+	@Test
+	public void insertAndVerifyUser2() throws DuplicateEmailException {
+		String firstName = "firstName";
+		String lastName = "lastName";
+		String email = "email";
+		String telephone = "telephone";
+		Boolean isProfessional = false;
+		String password = "abc345";		
+		String countryCode = "AR";		
+		SocialMediaService signInProvider = SocialMediaService.FACEBOOK;
+		
+		RegistrationDTO userAccountData = RegistrationDTO.newInstance().withEmail(email)
+		.withFirstName(firstName).withLastName(lastName).withPassword(password)
+		.withIsProfessional(isProfessional).withTelephone(telephone).withCountryCode(countryCode).withSignInProvider(signInProvider)
+		.build();
+		User response = userService.registerNewUserAccount(userAccountData);
+
+		UserDTO userRetrieved = userService.get(response.getId());
 		Assert.assertNotNull(userRetrieved);
 
 		Assert.assertEquals(email, userRetrieved.getEmail());
-	}
+	}	
 }
